c333c89ea581fca00069cfb6d3c4188e781f4888
jui1 <juimandal31@gmail.com>
2025-12-09 20:37:12 +0530

    first commit

---
+++ Reverse.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Reverse {
+    public static void main(String[] args) {
+        int arr [] = {8 , 9 , -1 , 9 , -55};
+
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+
+            start++;
+            end--;
+
+        }
+
+
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
